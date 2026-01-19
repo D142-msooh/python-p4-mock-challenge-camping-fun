1e6ad7bd4b815af37a782b967d7bf962119b7fe5
D142-msooh <kinyajuidama176@gmail.com>
2026-01-19 07:19:03 +0300

    Add app.py

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from models import db, Activity, Camper, Signup
-from flask_restful import Api, Resource
 from flask_migrate import Migrate
 from flask import Flask, make_response, jsonify, request
 import os
